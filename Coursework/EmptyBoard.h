010b518c4cb0ec58204aeb3835f8629502020fa2
Jack Millar <jackomillar@outlook.com>
2024-02-01 14:36:31 +0000

    Put empty board generator into header file

--- Coursework/EmptyBoard.h
+++ Coursework/EmptyBoard.h
@@ -2,23 +2,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+void empty_board(int sud[9][9]);
 
 void empty_board(int sud[9][9])
 {
-   for (int i = 0; i < 9; i++)
-   {
-     printf("%d %d %d | %d %d %d | %d %d %d", sud[1][1], sud[1][1], sud[1][1], sud[1][1], sud[1][1], sud[1][1], sud[1][1], sud[1][1], sud[1][1]);
-   }
-   /*
-	printf("X X X | X X X | X X X\n");
-    printf("X X X | X X X | X X X\n");
-    printf("X X X | X X X | X X X\n");
-    printf("------+-------+------\n");
-	printf("X X X | X X X | X X X\n");
-    printf("X X X | X X X | X X X\n");
-    printf("X X X | X X X | X X X\n");
-    printf("------+-------+------\n");
-	printf("X X X | X X X | X X X\n");
-    printf("X X X | X X X | X X X\n");
-    printf("X X X | X X X | X X X\n");*/
+for (int i = 0; i < 9; i++)
+{
+    for (int j = 0; j < 9; j++)
+    {
+        if (j % 3 == 0 && j != 0)
+        {
+            printf("| ");
+        }
+        sud[i][j] = 0;
+        printf("%d " , sud[i][j]);
+    }
+    printf("\n");
+    if (i % 3 == 2 && i != 8)
+    {
+        printf("------+-------+------");
+        printf("\n");
+    }
+
+}
 }
--- Coursework/Main.c
+++ Coursework/Main.c
@@ -13,25 +13,5 @@ int main ( int argc , char **argv )
     //struct Sud_Values plot[9];
     int sud[9][9];
 
-for (int i = 0; i < 9; i++)
-{
-    for (int j = 0; j < 9; j++)
-    {
-        if (j % 3 == 0 && j != 0)
-        {
-            printf("| ");
-        }
-        sud[i][j] = 0;
-        printf("%d " , sud[i][j]);
-    }
-    printf("\n");
-    if (i % 3 == 2 && i != 8)
-    {
-        printf("------+-------+------");
-        printf("\n");
-    }
-    
-}
-
-    //empty_board(sud);
+    empty_board(sud);
 }
