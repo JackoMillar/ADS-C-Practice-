9527b4dbf4bb54b0012f8f66e119de4a09008d40
Jack Millar <jackomillar@outlook.com>
2024-02-01 17:22:16 +0000

    Board with unique rows

--- Coursework/EmptyBoard.h
+++ Coursework/EmptyBoard.h
@@ -6,23 +6,26 @@ void empty_board(int sud[9][9]);
 
 void empty_board(int sud[9][9])
 {
-for (int i = 0; i < 9; i++)
-{
-    for (int j = 0; j < 9; j++)
+    for (int i = 0; i < 9; i++)
     {
-        if (j % 3 == 0 && j != 0)
+        for (int j = 0; j < 9; j++)
         {
-            printf("| ");
+            // Print all vertical lines
+            if (j % 3 == 0 && j != 0)
+            {
+                printf("| ");
+            }
+            // Print each individual value
+            printf("%d " , sud[i][j]);
         }
-        sud[i][j] = 0;
-        printf("%d " , sud[i][j]);
-    }
-    printf("\n");
-    if (i % 3 == 2 && i != 8)
-    {
-        printf("------+-------+------");
+        // Line Break
         printf("\n");
+        
+        // Print horizontal lines
+        if (i % 3 == 2 && i != 8)
+        {
+            printf("------+-------+------");
+            printf("\n");
+        }
     }
-
-}
 }
--- Coursework/Main.c
+++ Coursework/Main.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 #include "EmptyBoard.h"
-
-struct Sud_Values
-{
-    int vertical[9];
-    int horizontal[9];
-};
 
 int main ( int argc , char **argv )
 {
-    //struct Sud_Values plot[9];
+    // Init int array for holding sudoku values
     int sud[9][9];
+    int temp = 0;
+    bool check = false;
 
+  // Initialize random seed
+    srand(time(NULL));
+
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            do {
+                temp = (rand() % 9) + 1;
+                check = false;
+
+                // Check if the value is already present in the row
+                for (int k = 0; k < j; k++) {
+                    if (sud[i][k] == temp) {
+                        check = true;
+                        break;
+                    }
+                }
+            } while (check);
+
+            sud[i][j] = temp;
+        }
+    }
+
+    // Generate empty
     empty_board(sud);
 }
