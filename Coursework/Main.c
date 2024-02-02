c253937414c36017878d5e30afe7ddab4e6bbc65
Jack Millar <jackomillar@outlook.com>
2024-02-02 11:27:03 +0000

    Timing

--- Coursework/Main.c
+++ Coursework/Main.c
@@ -107,10 +107,15 @@ void generateSud(int sud[9][9])
 
 int main ( int argc , char **argv )
 {
+    clock_t t;
+    printf("start: %d \n", (int) (t=clock()));
+
     // Init int array for holding sudoku values
     int sud[9][9] = {0};
     generateSud(sud);
 
     empty_board(sud);
 
+    printf("stop: %d \n", (int) (t=clock()-t));
+    printf("Elapsed: %f seconds\n", (double) t / CLOCKS_PER_SEC);
 }
